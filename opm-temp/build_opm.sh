bdbbe27d89255fa11d47f41eca90b3d1bda0f17b
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2022-11-17 09:03:14 +0100

    added scripts

---
+++ opm-temp/build_opm.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+set -e
+CC=$(which gcc-11)
+CXX=$(which g++-11)
+location=$(pwd)
+parallel_build_tasks=4
+cd $location
+
+for repo in opm-common opm-material opm-grid opm-models opm-simulators
+do
+    cd $location
+    if [[ ! -d $repo ]]; then
+        if [[ $repo != 'opm-simulators' ]]; then
+            git clone https://github.com/OPM/$repo.git
+            cd $repo
+            git checkout `git rev-list -n 1 --first-parent --before="2022-11-06 13:37" master`
+            cd -
+        else
+            git clone https://github.com/atgeirr/opm-simulators -b write-global-cell-index-to-damaris
+	        cp opm-simulators_CMakeLists.txt ./opm-simulators/CMakeLists.txt
+            cp opm-simulators-prereqs.cmake ./opm-simulators/
+        fi
+    fi
+    cd $repo
+    if [[ ! -d build ]]; then
+        mkdir build
+    fi
+    cd build
+    USE_DAMARIS=''
+    TESTING=''
+    if [[ $repo == 'opm-simulators' ]]; then
+        USE_DAMARIS='-DUSE_DAMARIS_LIB=ON'
+        TESTING='-DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF'
+    fi
+    cmake -DCMAKE_C_COMPILER=$CC \
+        -DCMAKE_CXX_COMPILER=$CXX \
+        -DUSE_MPI=1  \
+        -DCMAKE_PREFIX_PATH="$location/damaris-install/;$location/zoltan/;$location/dune;$location/boost;$location/opm-common;$location/opm-material;$location/opm-grid;$location/opm-models" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -Wno-dev \
+        ${USE_DAMARIS} \
+        -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF \
+        ..
+    make -j$parallel_build_tasks
+    #make install
+done
---
+++ opm-temp/opm-simulators-prereqs.cmake
@@ -0,0 +1,55 @@
+# defines that must be present in config.h for our headers
+set (opm-simulators_CONFIG_VAR
+  HAVE_OPM_GRID
+  HAVE_PTHREAD
+  HAVE_EWOMS
+  HAVE_MPI
+  HAVE_PETSC
+  HAVE_CUDA
+  HAVE_OPENCL
+  HAVE_OPENCL_HPP
+  HAVE_FPGA
+  HAVE_AMGCL
+  HAVE_VEXCL
+  HAVE_SUITESPARSE_UMFPACK_H
+  HAVE_DUNE_ISTL
+  DUNE_ISTL_WITH_CHECKING
+  DUNE_ISTL_VERSION_MAJOR
+  DUNE_ISTL_VERSION_MINORm
+  DUNE_ISTL_VERSION_REVISION
+  HAVE_SUITESPARSE_UMFPACK
+  HAVE_DAMARIS
+  )
+
+# dependencies
+set (opm-simulators_DEPS
+  # Compile with C99 support if available
+  "C99"
+  # Various runtime library enhancements
+  "Boost 1.44.0
+    COMPONENTS date_time system unit_test_framework numpy REQUIRED"
+  # DUNE prerequisites
+  "dune-common REQUIRED"
+  "dune-istl REQUIRED"
+  # matrix library
+  "BLAS REQUIRED"
+  "LAPACK REQUIRED"
+  # Look for MPI support
+  "MPI"
+  # Tim Davis' SuiteSparse archive
+  "SuiteSparse REQUIRED COMPONENTS umfpack"
+  # SuperLU direct solver
+  "SuperLU"
+  # OPM dependency
+  "opm-common REQUIRED"
+  "opm-material REQUIRED"
+  "opm-grid REQUIRED"
+  "opm-models REQUIRED"
+  "Damaris REQUIRED"
+  )
+
+find_package_deps(opm-simulators)
+
+if(NOT HAVE_ECL_INPUT OR NOT HAVE_ECL_OUTPUT)
+  message(FATAL_ERROR "Eclipse input/output support required in opm-common")
+endif()
