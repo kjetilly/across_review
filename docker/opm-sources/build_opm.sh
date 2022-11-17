70e1ed34fa04f867bcb4fd02dae9d3985cf89e73
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2022-11-17 18:14:10 +0100

    updates to dokcerfile

--- docker/opm-sources/build_opm.sh
+++ docker/opm-sources/build_opm.sh
@@ -36,7 +36,7 @@ do
     cmake -DCMAKE_C_COMPILER=$CC \
         -DCMAKE_CXX_COMPILER=$CXX \
         -DUSE_MPI=1  \
-        -DCMAKE_PREFIX_PATH="${BUILD_FOLDER}/damaris-install;$location/zoltan/;$location/dune;$location/boost;$location/opm-common;$location/opm-material;$location/opm-grid;$location/opm-models" \
+        -DCMAKE_PREFIX_PATH="$location/damaris-install;$location/zoltan/;$location/dune;$location/boost;$location/opm-common;$location/opm-material;$location/opm-grid;$location/opm-models" \
         -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_EXAMPLES=OFF \
         -DBUILD_TESTING=OFF \
