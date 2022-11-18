73f30f94285be6b50a19f3b5e1ce439e58ab0cc8
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2022-11-18 13:29:43 +0100

    even more fixes to dockerfile

--- docker/run-scripts/run_ert.sh
+++ docker/run-scripts/run_ert.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # First we start dask
-dask-scheduler --scheduler-file ${DASK_FILE} &
+location=$(pwd)
+dask-scheduler --scheduler-file ${DASK_FILE} &> dask_log.txt &
 
 # Then the Bokeh server
-bokeh serve main.py --args -s ${DASK_FILE} &
-
-# Then we can start ERT
+cd /damaris-scripts/
+bokeh serve main.py --args -s ${DASK_FILE} &> $location/bokeh_log.txt &
+cd -
+sleep 5s
+# Then we can start flow
+export PYTHONPATH=$PYTHONPATH:/opt/src/damaris_python
 ert "$@"
