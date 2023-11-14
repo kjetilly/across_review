6845f3739e25347a3e117d986ea5f25c47f90793
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-11-14 14:18:41 +0100

    Now specifying commits for opm for damaris

--- docker/opm-sources-master/build_opm_josh.sh
+++ docker/opm-sources-master/build_opm_josh.sh
@@ -17,6 +17,10 @@ install_prefix=$location/opm-install-josh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 for repo in opm-common opm-grid opm-models opm-simulators
 do
+	# opm-common: 9e3fa794cebb3f0902dfa3400797254ad9ddd5ce
+	# opm-grid: 0276599f9eb63de44c3e4f1560f134d8ff77c03d
+	# opm-models: 24a8280933230c22b030de0ea73cb0e6109e51f9
+	# opm-simulators: 40697789f836da7a5eab4e3f54733e204856cc64  (from github/jcbowden)
     cd $location
     if [[ ! -d $repo ]]; then
 		if [[ "$repo" == "opm-simulators" ]]
@@ -29,7 +33,18 @@ do
 		else
 			git clone https://github.com/OPM/${repo}
 			cd ${repo}
-			git checkout `git rev-list -n 1 --before="2023-09-28 18:37" master`
+			if [[ "$repo" == "opm-common" ]]
+			then
+				git checkout 9e3fa794cebb3f0902dfa3400797254ad9ddd5ce
+			elif [[ "$repo" == "opm-grid" ]]
+			then
+				git checkout 0276599f9eb63de44c3e4f1560f134d8ff77c03d
+			elif [[ "$repo" == "opm-models" ]]
+			then
+				git checkout 24a8280933230c22b030de0ea73cb0e6109e51f9
+			else
+				git checkout `git rev-list -n 1 --before="2023-09-28 18:37" master`
+			fi
 			cd ..;
 		fi
     fi
