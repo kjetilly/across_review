e35a12bbc18c987a2fb84d05fb8083a54b87db48
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-11-14 14:12:00 +0100

    Updated Karolina build script

--- workflow_bash/install_components_karolina.sh
+++ workflow_bash/install_components_karolina.sh
@@ -24,28 +24,32 @@ mkdir -p ${BUILD_FOLDER}
 BUILD_TYPE=Release
 
 python -m venv $ERT_VENV
-python -m venv $FLOW_VENV
+python -m venv  --system-site-packages $FLOW_VENV
 
-. $ERT_VENV/bin/activate 
-pip install --upgrade pip
-cd ${BUILD_FOLDER}
-pip install --upgrade pip wheel setuptools
+#. $ERT_VENV/bin/activate 
+#pip install --upgrade pip
+#cd ${BUILD_FOLDER}
+#pip install --upgrade pip wheel setuptools
 #pip install cmake==3.16.8
-pip install conan==1.61.0
+#pip install conan==1.61.0
 #conan profile detect
-pip install git+https://github.com/kjetilly/ert.git@hq
-deactivate
+#pip install git+https://github.com/kjetilly/ert.git@hq
+#deactivate
+
+
+
 
 . $FLOW_VENV/bin/activate
 pip install --upgrade pip
-pip3 install -r ${docker_dir}/damaris-scripts/requirements_karolina.txt
+pip3 install -r ${docker_dir}/damaris-scripts/requirements.txt
+pip3 install cmake
 
 # we need build2 for xsd which we in turn need for damaris
 cd $BUILD_FOLDER/
 mkdir build2
 cd build2
 curl -sSfO https://download.build2.org/0.16.0/build2-install-0.16.0.sh
-sh build2-install-0.16.0.sh --yes ${target_dir}/build2
+sh build2-install-0.16.0.sh --trust 70:64:FE:E4:E0:F3:60:F1:B4:51:E1:FA:12:5C:E0:B3:DB:DF:96:33:39:B9:2E:E5:C2:68:63:4C:A6:47:39:43 --yes ${target_dir}/build2
 
 # We need XSD for damaris
 cd ${BUILD_FOLDER}
@@ -59,10 +63,12 @@ ${target_dir}/build2/bin/bpkg create -d xsd-gcc-10 cc     \
   config.install.root=${target_dir}/xsd-install 
 
 cd xsd-gcc-10
-${target_dir}/build2/bin/bpkg build --yes --sys-yes xsd@https://pkg.cppget.org/1/beta
+${target_dir}/build2/bin/bpkg build --trust 70:64:FE:E4:E0:F3:60:F1:B4:51:E1:FA:12:5C:E0:B3:DB:DF:96:33:39:B9:2E:E5:C2:68:63:4C:A6:47:39:43 --yes --sys-yes xsd@https://pkg.cppget.org/1/beta
+
 ${target_dir}/build2/bin/bpkg install xsd
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${target_dir}/xsd-install/lib
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${target_dir}/xsd-install/lib:${target_dir}/boost-install
 
 cd ${BUILD_FOLDER}/xsd
 
@@ -73,13 +79,17 @@ ${target_dir}/build2/bin/bpkg create -d libxsd-gcc-10 cc  \
 
 cd libxsd-gcc-10
 ${target_dir}/build2/bin/bpkg add https://pkg.cppget.org/1/beta
-${target_dir}/build2/bin/bpkg fetch
-${target_dir}/build2/bin/bpkg build libxerces-c
-${target_dir}/build2/bin/bpkg build libexpat
-${target_dir}/build2/bin/bpkg build libxsd
+${target_dir}/build2/bin/bpkg fetch --trust 70:64:FE:E4:E0:F3:60:F1:B4:51:E1:FA:12:5C:E0:B3:DB:DF:96:33:39:B9:2E:E5:C2:68:63:4C:A6:47:39:43
+${target_dir}/build2/bin/bpkg build --yes libxerces-c
+${target_dir}/build2/bin/bpkg build --yes libexpat
+${target_dir}/build2/bin/bpkg build --yes libxsd
 ${target_dir}/build2/bin/bpkg install --all --recursive
 
+deactivate
 cd ${BUILD_FOLDER}
+bash ${SCRIPT_DIR}/install_boost.sh ${target_dir}/boost-install
+cd ${BUILD_FOLDER}
+. $FLOW_VENV/bin/activate
 mkdir -p ${target_dir}/damaris-extra
 
 git clone https://gitlab.inria.fr/Damaris/damaris.git
@@ -90,7 +100,7 @@ cmake .. \
     -DENABLE_HDF5=ON \
     -DENABLE_PYTHON=ON \
     -DENABLE_PYTHONMOD=ON \
-    -DCMAKE_PREFIX_PATH=${target_dir}/xsd-install \
+    -DCMAKE_PREFIX_PATH="${target_dir}/xsd-install;${target_dir}/boost-install" \
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
     -DGENERATE_MODEL=ON \
     -DPython_FIND_VIRTUALENV=ONLY \
@@ -105,12 +115,19 @@ git clone https://github.com/kjetilly/opm-runner.git
 cp -r ${docker_dir}/opm-sources-master ${BUILD_FOLDER}/opm-sources
 cd ${BUILD_FOLDER}
 cd opm-sources
-bash build_zoltan.sh
-bash build_dune.sh
-bash build_opm_component.sh opm-common
-bash build_opm_component.sh opm-grid
-bash build_opm_component.sh opm-models
-bash build_opm_component.sh opm-simulators
+bash build_zoltan.sh ${target_dir}/boost-install
+bash build_dune.sh ${target_dir}/boost-install
+bash build_opm_component.sh opm-common ${target_dir}/boost-install
+bash build_opm_component.sh opm-grid ${target_dir}/boost-install
+bash build_opm_component.sh opm-models ${target_dir}/boost-install
+bash build_opm_component.sh opm-simulators ${target_dir}/boost-install
+
+cd ${BUILD_FOLDER}
+mkdir -p opm-josh
+cd opm-josh
+bash ../opm-sources/build_zoltan.sh ${target_dir}/boost-install
+bash ../opm-sources/build_dune.sh ${target_dir}/boost-install
+bash ../opm-sources/build_opm_josh.sh ${target_dir}/boost-install
 deactivate
 
 
