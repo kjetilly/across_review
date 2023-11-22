775b126c86f9a00bae6747b448d75d8305ecd601
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-11-22 18:31:16 +0100

    Now specifying number of cores for fake ert

--- workflow_bash/fake_ert.sh
+++ workflow_bash/fake_ert.sh
@@ -6,4 +6,5 @@ python \
     $FAKE_ERT_SCRIPT \
     --outputdir outdirert \
     --ert-file $(realpath $(basename $1)) \
-    --flowpath $(which flow_venv.sh)
+    --flowpath $(which flow_venv.sh) \
+    --cpus-per-sample "${FAKE_ERT_NUM_PROCS:-2}"
