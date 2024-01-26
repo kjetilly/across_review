8f6bcd369856785ffeef829ba290510560ac0b05
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-01-26 13:17:59 +0100

    Added runscript for SPE11B

---
+++ runall_spe11b.sh
@@ -0,0 +1,14 @@
+# Yes, we still call this SPE11C_RESOLUTION
+export ACROSS_SPE11C_RESOLUTION='840,1,120' 
+
+cd /home/kjetil/projects/across/workflow/across_review/;
+./installdir/bin/hq server stop --server-dir /home/kjetil/.hq-server
+killall dask-scheduler
+killall dask-worker
+ps aux|grep machine_learning_across.py|awk '{system("kill -9 "$2)}'
+rm -rf outputdir_spe11
+mkdir -p outputdir_spe11
+cd outputdir_spe11
+cp -r ../spe11b ./spe11b
+bash ../workflow_bash/run_workflow_from_spe11.sh ../installdir/ $(realpath spe11b)
+cd -
