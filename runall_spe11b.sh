cb2762937e55037ee9d17ea8fa692547957c553b
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-01-26 13:33:20 +0100

    fixes for spe11b

--- runall_spe11b.sh
+++ runall_spe11b.sh
@@ -6,9 +6,9 @@ cd /home/kjetil/projects/across/workflow/across_review/;
 killall dask-scheduler
 killall dask-worker
 ps aux|grep machine_learning_across.py|awk '{system("kill -9 "$2)}'
-rm -rf outputdir_spe11
-mkdir -p outputdir_spe11
-cd outputdir_spe11
+rm -rf outputdir_spe11b
+mkdir -p outputdir_spe11b
+cd outputdir_spe11b
 cp -r ../spe11b ./spe11b
 bash ../workflow_bash/run_workflow_from_spe11.sh ../installdir/ $(realpath spe11b)
 cd -
